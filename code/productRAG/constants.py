86e873b4cc01330e9bb5902096a388752480776c
maddy-git <madhav.venugopalan@gmail.com>
2025-11-06 00:18:42 +0530

    changes

--- code/productRAG/constants.py
+++ code/productRAG/constants.py
@@ -18,6 +18,6 @@ reranker_prompt = rerankerPrompts.rerank_prompt_creative
 refine_prompt_name = "refine_prompt_creative"
 reranker_prompt_name = "rerank_prompt_creative"
 
-#os.environ["GOOGLE_API_KEY"] = "AIzaSyBfCV-TobP0xCdhrLvcZwbjSrIWjo03CX8"
-os.environ["GOOGLE_API_KEY"] = "AIzaSyDsqn5BtaPWEI4HE_4gAANWVSpMlxClE8Y"
+#os.environ["GOOGLE_API_KEY"] = ""
+os.environ["GOOGLE_API_KEY"] = ""
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=temp)
