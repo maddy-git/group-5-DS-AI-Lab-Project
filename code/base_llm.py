2919c8909d88f8af8be37a4374c647dc6c134a4f
maddy-git <madhav.venugopalan@gmail.com>
2025-11-06 00:18:24 +0530

    changes

--- code/base_llm.py
+++ code/base_llm.py
@@ -1,5 +1,5 @@
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-os.environ["GOOGLE_API_KEY"] = "AIzaSyD06a9SVvel8XDgn02HdKgJGMnSJMS_TrA"
+os.environ["GOOGLE_API_KEY"] = ""
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0)
