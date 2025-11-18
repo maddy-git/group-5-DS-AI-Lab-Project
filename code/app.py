9e34072f04a38ad1ad91716bf00133a274792dd2
ManishS256 <84698392+ManishS256@users.noreply.github.com>
2025-11-18 12:16:59 +0530

    Change app run configuration to host and port

--- code/app.py
+++ code/app.py
@@ -13,4 +13,4 @@ def handle_query():
     return jsonify({"response": llm_response, "images":images}),200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
